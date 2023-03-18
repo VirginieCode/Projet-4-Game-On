eaad04f9a12dd3770e8efa1dda395f167b6e968a
VirginieCode <dmy.virginie@gmail.com>
2023-03-18 17:16:21 +0000

    Ajout de la boucle pour vérifier si une des checkbox location est checkée

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -32,6 +32,9 @@ function stopModal() {
 //Les champs du formulaire
 
 const formDataInputs = document.querySelectorAll("input");
+const locationCheckbox = document.querySelectorAll(
+  '.checkbox-input[type="radio"]'
+);
 const conditionsCheckbox = document.getElementById("checkbox1");
 
 //Création des messages d'erreurs qui s'affichent lorsque un inputs est incorrect
@@ -44,9 +47,8 @@ let errorMsg = {
   quantity:
     "Veuillez indiquer le nombre de tournoi auxquelles vous avez participé",
   location: "Veuillez cocher le lieu",
+  conditions: "Veuillez coché la case pour accepter les conditions",
 };
-
-
 //Envoie du formulaire
 
 const submitBtn = document.querySelector(".btn-submit");
@@ -65,7 +67,7 @@ function submitForm(e) {
 function validation() {
   let allInputs = true; //Je set la valeur true à mes inputs
 
-//Création d'une boucle for qui va vérifier si la valeur de mes inputs n'est pas vide
+  //Création d'une boucle for qui va vérifier si la valeur de mes inputs n'est pas vide
 
   for (let i = 0; i < formDataInputs.length; i++) {
     if (formDataInputs[i].value === "") {
@@ -80,9 +82,9 @@ function validation() {
       } // ici je précise que si il y a déjà un message d'erreur alors il faut supprimer le précédant à fin que il n'y en ai que un, et que il ne se rajoute à l'autre
 
       let errorDiv = document.createElement("div"); //Création de la div pour le msg d'erreur
-     
+
       divDataForm.appendChild(errorDiv); //Ajout du message à la div
-      divDataForm.setAttribute("data-error-visible","true")
+      divDataForm.setAttribute("data-error-visible", "true");
       divDataForm.dataset.error = error; // Application de l'attribue data-error
       errorDiv.classList.add("errorMsg"); //J'attribue une class au msg d'erreur
 
@@ -92,82 +94,85 @@ function validation() {
     //Création d'un addEventListener qui va écouter les inputs et supprimer le message d'error dés que la valeur de l'input est modifiée
 
     formDataInputs[i].addEventListener("input", function () {
-      
       let newDivDataForm = this.closest(".formData"); //div la plus proche de l'input
 
-     
       let existingError = newDivDataForm.querySelector(".errorMsg"); //recherche du message d'erreur dans la div la plus proche
 
-      
       if (existingError) {
         existingError.remove(); //Si message d'erreur existant, il faut le supprimer
-        newDivDataForm.setAttribute("data-error-visible","false") 
+        newDivDataForm.setAttribute("data-error-visible", "false");
       }
     });
 
-    
-//Création d'une boucle for qui va vérifier si la ma checkbox condition est bien cochée
+    //Vérification pour voir si un lieu a bien été coché
 
-    if (!conditionsCheckbox.checked) {
-
-     // const checkboxErrorMsg =
-       // "Veuillez cocher la case pour accepter les conditions"; // Création du message d'erreur
-      let checkboxInputDiv = conditionsCheckbox.closest(".formData");
-      let existingError = checkboxInputDiv.querySelector(".error-checkbox");
-      if (existingError) {
-        existingError.remove();
+    for (let i = 0; i < locationCheckbox.length; i++) {
+      if (locationCheckbox[i].checked) {
+        allInputs = true;
+        break;
       }
-      let errorcheckboxDiv = document.createElement("div"); 
-     // errorcheckboxDiv.innerText = checkboxErrorMsg;
-     checkboxInputDiv.setAttribute("data-error-visible","true")
-      checkboxInputDiv.dataset.error = "Veuillez cocher la case pour accepter les conditions";
-      errorcheckboxDiv.classList.add("errorMsg");
-      checkboxInputDiv.appendChild(errorcheckboxDiv); //Je relie ma div contenant le message d'erreur à la div la plus proche de ma checkbox
-      allInputs = false;
+
+      if (!locationCheckbox[i].checked) {
+        let divDataFormLocation = locationCheckbox[i].closest(".formData");
+        let errorExisting = divDataFormLocation.querySelector(".errorMsg");
+        let errorDivLocation = document.createElement("div"); 
+        divDataFormLocation.appendChild(errorDivLocation); 
+        divDataFormLocation.setAttribute("data-error-visible", "true");
+        let inputNameattr = locationCheckbox[i].getAttribute("name"); 
+        const error = errorMsg[inputNameattr];
+        divDataFormLocation.dataset.error = error; 
+        errorDivLocation.classList.add("errorMsg"); 
+
+        if (errorExisting) {
+          errorExisting.remove();
+        }
+
+        allInputs = false;
+        
+      }
     }
 
-     //Création d'un addEventListener qui va écouter les inputs et supprimer le message d'error dés que la valeur de l'input est modifiée
+    locationCheckbox[i].addEventListener("input", function () {
+      let DivDataForm = this.closest(".formData");
+
+      let existingError = DivDataForm.querySelector(".errorMsg");
+
+      if (existingError) {
+        existingError.remove();
+        DivDataForm.setAttribute("data-error-visible", "false");
+      }
+    });
+
+    //Vérification pour voir si la case conditions est bien coché
+
+    if (!conditionsCheckbox.checked) {
+      let checkboxInputDiv = conditionsCheckbox.closest(".formData");
+      let errorcheckboxDiv = document.createElement("div");
+
+      checkboxInputDiv.setAttribute("data-error-visible", "true");
+      let inputNameattr = conditionsCheckbox.getAttribute("name"); 
+      const error = errorMsg[inputNameattr];
+      checkboxInputDiv.dataset.error = error;
+ 
+      errorcheckboxDiv.classList.add("errorMsg");
+      checkboxInputDiv.appendChild(errorcheckboxDiv); 
+      allInputs = false;
+
+    }
+
+    //Création d'un addEventListener qui va écouter les inputs et supprimer le message d'error dés que la valeur de l'input est modifiée
 
     conditionsCheckbox.addEventListener("click", function () {
-      
-      let thisDivDataForm = this.closest(".formData"); 
-
+      let thisDivDataForm = this.closest(".formData");
 
       let existingError = thisDivDataForm.querySelector(".errorMsg");
 
-    
       if (conditionsCheckbox.checked) {
         existingError.remove();
-        thisDivDataForm.setAttribute("data-error-visible","false") 
+        thisDivDataForm.setAttribute("data-error-visible", "false");
       }
     });
   }
-
-  /*
-  function locationValidation() {
-    if (
-      document.getElementById("location1").checked ||
-      document.getElementById("location2").checked ||
-      document.getElementById("location3").checked ||
-      document.getElementById("location4").checked ||
-      document.getElementById("location5").checked ||
-      document.getElementById("location6").checked
-    ) {
-      return true;
-    } else {
-      const locationError = document.querySelector(".text-label");
-
-      locationError.style.color = "red";
-
-      const locationErrorDiv = document.querySelector(".formData:nth-child(6)");
-
-      //locationErrorDiv.dataset.error = "Veuillez cocher une option";
-      return false;
-    }
-  }
-
-
-  */
 
   // Ici je verifie si tous les inputs sont true, si pas d'erreur lancement de la fonction validate
   if (allInputs) {
@@ -220,7 +225,3 @@ function validation() {
     }
   }
 }
-
-
-
-
